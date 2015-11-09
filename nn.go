63f026df6896faf0f0e8d01b452c4bc547865a65
Garret Raziel <boloomka@gmail.com>
2015-11-09 22:24:53 +0100

    add train method

--- nn.go
+++ nn.go
@@ -2,8 +2,21 @@ package nn
 
 import (
     "strconv"
+    "math/rand"
     "github.com/garretraziel/matrices"
 )
+
+// TrainItem represents one item for training of neural network
+type TrainItem struct {
+    Values matrices.Matrix
+    Label float64
+}
+
+// InitTrainItem initializes new training item - values and label
+func InitTrainItem(values []float64, label float64) (TrainItem, error) {
+    matrix, err := matrices.InitMatrixWithValues(1, len(values), values)
+    return TrainItem{matrix, label}, err
+}
 
 // NN represents neural network to be used with backpropagation
 type NN struct {
@@ -67,21 +80,64 @@ func (network NN) FeedForward(input matrices.Matrix) matrices.Matrix {
 }
 
 // Evaluate returns ratio of correctly clasified inputs
-func (network NN) Evaluate(inputs []matrices.Matrix, labels []float64) float64 {
+func (network NN) Evaluate(inputs []TrainItem) float64 {
     correct := 0
-    for i, input := range inputs {
-        output := network.FeedForward(input)
+    for _, input := range inputs {
+        output := network.FeedForward(input.Values)
         max, err := output.MaxAt()
         if err != nil {
             panic(err)
         }
-        if float64(max) == labels[i] {
+        if float64(max) == input.Label {
             correct++
         }
     }
-    return float64(correct) / float64(len(labels))
+    return float64(correct) / float64(len(inputs))
 }
 
-// func (network NN) Train(inputs []matrices.Matrix, labels matrices.Matrix, epochs int, mini_batch_size int, eta float64) {
-//
-// }
+// Train trains Network on given input with given settings
+func (network NN) Train(inputs []TrainItem, epochs int, miniBatchSize int, eta float64) {
+    inputCount := len(inputs)
+    for i := 0; i < epochs; i++ {
+        shuffled := make([]TrainItem, inputCount)
+        perm := rand.Perm(inputCount)
+        for i, v := range perm {
+            shuffled[v] = inputs[i]
+        }
+
+        batchesCount := int(float64(inputCount) / float64(miniBatchSize) + 0.5)
+        batches := make([][]TrainItem, batchesCount)
+        for i := 0; i < batchesCount; i++ {
+            if i + miniBatchSize >= inputCount {
+                batches[i] = inputs[i*miniBatchSize:]
+            } else {
+                batches[i] = inputs[i*miniBatchSize:i*miniBatchSize + miniBatchSize]
+            }
+        }
+
+        for _, batch := range batches {
+            network.updateMiniBatch(batch, eta)
+        }
+    }
+}
+
+func (network NN) updateMiniBatch(batch []TrainItem, eta float64) {
+    cxw := make([]matrices.Matrix, len(network.weights))
+    cxb := make([]matrices.Matrix, len(network.biases))
+    for i, m := range network.weights {
+        cxw[i] = matrices.InitMatrix(m.Rows(), m.Cols())
+    }
+    for i, m := range network.biases {
+        cxb[i] = matrices.InitMatrix(m.Rows(), m.Cols())
+    }
+
+    for _, item := range batch {
+        deltaW, deltaB := network.backprop(item)
+        cxw = cxw.Add(deltaW)
+        cxb = cxb.Add(deltaB)
+    }
+    for i, w := range cxw {
+
+        network.weights[i] = network.weights[i].Sub()
+    }
+}
